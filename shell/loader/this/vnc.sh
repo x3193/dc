6611648bd2a1ffd38bcf940e59c26f185b726e5c
x3193 <xcy6272003@126.com>
2016-07-17 19:08:44 +0800

    dc

--- shell/loader/this/vnc.sh
+++ shell/loader/this/vnc.sh
@@ -11,7 +11,7 @@ sudo service ssh start ;
 
 export LC_ALL='zh_CN.UTF-8' LANG='zh_CN.UTF-8' LANGUAGE='zh_CN:zh:en_US:en'
 TZ='Asia/Shanghai'; export TZ
-#tightvncserver :1
-#setsid /var/www/html/noVNC-master/utils/launch.sh --vnc localhost:5901 &
+tightvncserver :1
+setsid /var/www/html/noVNC-master/utils/launch.sh --vnc localhost:5901 &
 
 echo "--------------------VNC------------------------"
