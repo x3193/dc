da65d449c4711d6f041aecc088cba951dbf85116
x3193 <xcy6272003@126.com>
2016-08-26 23:44:56 +0800

    Update exp.sh

--- exp.sh
+++ exp.sh
@@ -1,4 +1,12 @@
 #/usr/bin/expect -f
 
 # run the application
-service ssh start 
+spawn su -
+expect -exact "password:"
+send "EUIfgwe7\r"
+expect "#"
+send "sh /run.sh\r"             
+expect "#"
+send "echo $?\r"
+sleep 5
+interact
