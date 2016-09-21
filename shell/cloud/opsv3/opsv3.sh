f5b16ccd55a6345e31ece263bebb250f1fe5caff
x3193 <xcy6272003@126.com>
2016-09-21 22:59:52 +0800

    Signed-off-by: x3193 <xcy6272003@126.com>

--- shell/cloud/opsv3/opsv3.sh
+++ shell/cloud/opsv3/opsv3.sh
@@ -303,6 +303,14 @@ fi
 echo "====="
 #fi
 
+sudo dpkg --add-architecture i386
+sudo dpkg --configure -a
+sudo dpkg-reconfigure -p high -f noninteractive debconf 
+sudo apt-get install -f
+sudo apt-get update -y
+sudo apt-get upgrade -y 
+sudo apt-get dist-upgrade -y 
+
 #dir
 #chown -R ${uid}:root /etc
 find /etc -name '*' -exec chown ${uid}:root {} \; 
